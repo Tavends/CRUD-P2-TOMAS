28738cc63356086a29c1fd138012a989da7c8da6
Tomas Avendaño Soriano <toavendano.dwfsc5@bootcampudd.cl>
2023-01-31 17:16:49 -0300

    adding proyect to the repository

---
+++ assets/index.js
@@ -0,0 +1,80 @@
+let items = [];
+const itemsKey = items
+
+const createForm = document.getElementById("create-form");
+const itemsTable = document.getElementById("items-table");
+const itemsTBody = document.getElementById("items-tbody");
+const updateForm = document.getElementById("update-form");
+const cancelUpdateBtn = document.getElementById("cancel-update-btn");
+
+
+/* Nota Personal: Con getElementById traemos todos los ID previamente creados para interactuar con ellos */
+
+createForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const itemName = document.getElementById("item-name").value;
+    const sector = document.getElementById("sector").value;
+    items.push({ id: Date.now(), itemName, sector });
+    localStorage.setItem(itemsKey, JSON.stringify(items));
+    renderItems(); /*Aqui creamos el formulario a leer*/
+    createForm.reset();
+});
+/* Nota Personal: Aqui utilizamos "createForm" para agregar los nuevos objetos al inventario */
+
+function renderItems() {
+    itemsTBody.innerHTML = "";
+    items.forEach(item => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td>${item.itemName}</td>
+            <td>${item.sector}</td>
+            <td>
+                <button data-id="${item.id}" class="update-btn">Update</button>
+                <button data-id="${item.id}" class="delete-btn">Delete</button>
+            </td>
+        `;
+        itemsTBody.appendChild(tr);
+    const getItems = () => {
+            const ItemList = 
+            localStorage.getItem (itemsKey)
+
+            items = JSON.parse(ItemList)
+            console.log("items", items);
+            
+        }
+    });
+}
+
+itemsTable.addEventListener("click", e => {
+    if (e.target.classList.contains("update-btn")) {
+        const id = e.target.dataset.id;
+        const item = items.find(item => item.id === Number(id));
+        document.getElementById("update-item-name").value = item.itemName;
+        document.getElementById("update-sector").value = item.sector;
+        document.getElementById("update-id").value = item.id;
+        document.getElementById("update-section").style.display = "block";
+    } else if (e.target.classList.contains("delete-btn")) {
+        const id = e.target.dataset.id;
+        items.splice(items.findIndex(item => item.id === Number(id)), 1);
+        renderItems();
+    }
+});
+
+updateForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const id = document.getElementById("update-id").value;
+    const itemName = document.getElementById("update-item-name").value;
+    const sector = document.getElementById("update-sector").value;
+    items.find(item => item.id === Number(id)).itemName = itemName;
+    items.find(item => item.id === Number(id)).sector = sector;
+    renderItems();
+    updateForm.reset();
+    document.getElementById("update-section").style.display = "none";
+});
+
+cancelUpdateBtn.addEventListener("click", e => {
+    updateForm.reset();
+    document.getElementById("update-section").style.display = "none";
+});
+
+renderItems();
